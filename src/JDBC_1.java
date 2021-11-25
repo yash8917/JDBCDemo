bb04a778a22c3207bb39174c69d31e9d1b773697
Yash Gupta <yash8917gupta@gmail.com>
2021-11-25 19:27:47 +0530

    Initial commit

---
+++ src/JDBC_1.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class JDBC_1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+//        oracle.jdbc.oracledriver
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Stu","root","");//Connection is interface
+        Statement s= con.createStatement();
+        ResultSet rs=s.executeQuery("select * from customer");
+
+        while(rs.next()){
+            System.out.println(rs.getString(1)+" "+rs.getInt(2));
+        }
+        s.close();
+        con.close();
+    }
+}
