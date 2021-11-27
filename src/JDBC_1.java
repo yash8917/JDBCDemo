1551d4d27a70fe8c30437a7417d43abbd72fab2c
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-27 19:55:04 +0530

    Update JDBC_1.java

--- src/JDBC_1.java
+++ src/JDBC_1.java
@@ -3,6 +3,7 @@ import java.sql.*;
 public class JDBC_1 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
+        
 //        oracle.jdbc.oracledriver
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Stu","root","");//Connection is interface
         Statement s= con.createStatement();
