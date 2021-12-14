aa40c08857a09272b7628a00f07c7516167806b9
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-12-14 21:20:24 +0530

    Update JDBC_1.java

--- src/JDBC_1.java
+++ src/JDBC_1.java
@@ -1,5 +1,4 @@
 import java.sql.*;
-
 public class JDBC_1 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
